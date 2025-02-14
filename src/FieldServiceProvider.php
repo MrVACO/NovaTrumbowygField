4675b1e87964fb0e6a5a4a3d066af3f742f4ee20
Mr.VACO <brute1904@gmail.com>
2025-02-14 18:37:00 +0500

    Added Nova version

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -10,12 +10,15 @@ class FieldServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Nova::serving(function(ServingNova $event)
-        {
+        Nova::serving(function (ServingNova $event) {
             $locale = app()->getLocale();
-            
+
             Nova::script('nova-trumbowyg-field-' . $locale, __DIR__ . '/../dist/js/field.' . $locale . '.js');
             Nova::style('nova-trumbowyg-field', __DIR__ . '/../dist/css/field.css');
         });
+
+        $this->publishes([
+            __DIR__ . '/../dist/images' => public_path('images'),
+        ], 'laravel-assets');
     }
 }
